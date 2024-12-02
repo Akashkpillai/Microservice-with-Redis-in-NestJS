9828080a2d89ca2eabd805c999764d52b8a30209
Akashkpillai <akashkpillai5@gmail.com>
2024-12-02 17:30:31 +0530

    changes

--- server/dating-app/user-service/src/user/user.controller.ts
+++ server/dating-app/user-service/src/user/user.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-} from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create.dto';
 import { User } from './dto/user.dto';
@@ -14,33 +6,30 @@ import { UpdateDto } from './dto/update.dto';
 
 @Controller('user')
 export class UserController {
-  constructor(private userService: UserService) {}
+    constructor(private userService: UserService) {}
 
-  @Post('sign-up')
-  async create(@Body() data: CreateUserDto): Promise<User> {
-    return this.userService.create(data);
-  }
+    @Post('sign-up')
+    async create(@Body() data: CreateUserDto): Promise<User> {
+        return this.userService.create(data);
+    }
 
-  @Patch(':id')
-  async update(
-    @Param('id') id: number,
-    @Body() data: UpdateDto,
-  ): Promise<{ message: string }> {
-    return this.userService.update(id, data);
-  }
+    @Patch(':id')
+    async update(@Param('id') id: number, @Body() data: UpdateDto): Promise<{ message: string }> {
+        return this.userService.update(id, data);
+    }
 
-  @Get()
-  async getAll(): Promise<User[]> {
-    return this.userService.findAll();
-  }
+    @Get()
+    async getAll(): Promise<User[]> {
+        return this.userService.findAll();
+    }
 
-  @Get(':id')
-  async findOne(@Param('id') id: number): Promise<User> {
-    return this.userService.findById(id);
-  }
+    @Get(':id')
+    async findOne(@Param('id') id: number): Promise<User> {
+        return this.userService.findById(id);
+    }
 
-  @Delete(':id')
-  async delete(@Param('id') id: number): Promise<boolean> {
-    return this.userService.delete(id);
-  }
+    @Delete(':id')
+    async delete(@Param('id') id: number): Promise<boolean> {
+        return this.userService.delete(id);
+    }
 }
--- server/dating-app/user-service/src/user/user.service.ts
+++ server/dating-app/user-service/src/user/user.service.ts
@@ -8,143 +8,128 @@ import { UpdateDto } from './dto/update.dto';
 
 @Injectable()
 export class UserService {
-  constructor(
-    private prismaService: PrismaService,
-    private utilsService: UtilsService,
-  ) {}
+    constructor(
+        private prismaService: PrismaService,
+        private utilsService: UtilsService
+    ) {}
 
-  async create(data: CreateUserDto): Promise<User> {
-    const existingUser = await this.utilsService.querySingle<User>(
-      `SELECT * FROM "user" WHERE email = $1 OR number = $2`,
-      data.email,
-      data.number,
-    );
+    async create(data: CreateUserDto): Promise<User> {
+        const existingUser = await this.utilsService.querySingle<User>(
+            `SELECT * FROM "user" WHERE email = $1 OR number = $2`,
+            data.email,
+            data.number
+        );
 
-    if (existingUser) {
-      const conflictField =
-        existingUser.email === data.email ? 'email' : 'number';
-      throw new HttpException(
-        { message: `User with same ${conflictField} already exist` },
-        HttpStatus.CONFLICT,
-      );
+        if (existingUser) {
+            const conflictField = existingUser.email === data.email ? 'email' : 'number';
+            throw new HttpException(
+                { message: `User with same ${conflictField} already exist` },
+                HttpStatus.CONFLICT
+            );
+        }
+
+        const hashedPassword = await this.hashPassword(data.password);
+
+        let query = 'INSERT INTO "user" (name, email, password ,number ,gender ,';
+        let values = `VALUES ('${data.name}', '${data.email}', '${hashedPassword}' ,${data.number},'${data.gender}',`;
+
+        if (data.bio) {
+            query += ' bio,';
+            values += ` '${data.bio}',`;
+        }
+
+        if (data.preferences) {
+            query += ' preferences';
+            values += `'${data.preferences}'`;
+        }
+
+        query += ') ';
+        values += ')';
+
+        const fullQuery = `${query} ${values} RETURNING id;`;
+
+        const user = await this.prismaService.$queryRawUnsafe(fullQuery);
+
+        if (user[0].id) {
+            const userData: User = await this.prismaService.$queryRawUnsafe(
+                ` SELECT * FROM "user" WHERE id = ${user[0].id}`
+            );
+            return userData;
+        } else {
+            console.error('Failed to create user or unexpected response:', user);
+            throw new HttpException(
+                { message: 'User creation failed' },
+                HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        }
     }
 
-    const hashedPassword = await this.hashPassword(data.password);
+    async update(id, payload: UpdateDto): Promise<{ message: string }> {
+        if (!payload) {
+            throw new HttpException(
+                { message: 'Please provide the data to be update' },
+                HttpStatus.BAD_REQUEST
+            );
+        }
 
-    let query = 'INSERT INTO "user" (name, email, password ,number ,gender ,';
-    let values = `VALUES ('${data.name}', '${data.email}', '${hashedPassword}' ,${data.number},'${data.gender}',`;
+        const existingUser = await this.findById(id);
 
-    if (data.bio) {
-      query += ' bio,';
-      values += ` '${data.bio}',`;
+        if (!existingUser) {
+            throw new HttpException({ message: 'User not found to update' }, HttpStatus.NOT_FOUND);
+        } else {
+            const updateResult = await this.utilsService.updateQuery(
+                'user', // Table name
+                payload, // Fields to update
+                { id: parseInt(id, 10) } // Condition for the update
+            );
+            return updateResult ? { message: 'User updated' } : { message: 'User updation failed' };
+        }
     }
 
-    if (data.preferences) {
-      query += ' preferences';
-      values += `'${data.preferences}'`;
+    async findAll(): Promise<User[]> {
+        const users = await this.prismaService.$queryRawUnsafe<User[]>(
+            `SELECT name , email , bio ,gender , preferences FROM "user";`
+        );
+        if (!users.length) {
+            throw new HttpException({ message: 'User details not found' }, HttpStatus.NOT_FOUND);
+        }
+        return users;
     }
 
-    query += ') ';
-    values += ')';
+    async findById(id): Promise<User> {
+        if (typeof id == 'string') {
+            id = parseInt(id, 10);
+        }
+        const user = await this.utilsService.querySingle<User>(
+            `SELECT id , name , email , number ,bio , gender ,preferences FROM "user" WHERE id = $1`,
+            id
+        );
 
-    const fullQuery = `${query} ${values} RETURNING id;`;
-
-    const user = await this.prismaService.$queryRawUnsafe(fullQuery);
-
-    if (user[0].id) {
-      const userData: User = await this.prismaService.$queryRawUnsafe(
-        ` SELECT * FROM "user" WHERE id = ${user[0].id}`,
-      );
-      return userData;
-    } else {
-      console.error('Failed to create user or unexpected response:', user);
-      throw new HttpException(
-        { message: 'User creation failed' },
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
-  }
-
-  async update(id, payload: UpdateDto): Promise<{ message: string }> {
-    if (!payload) {
-      throw new HttpException(
-        { message: 'Please provide the data to be update' },
-        HttpStatus.BAD_REQUEST,
-      );
+        if (!user) {
+            throw new HttpException({ message: 'User not found' }, HttpStatus.NOT_FOUND);
+        } else {
+            return user;
+        }
     }
 
-    const existingUser = await this.findById(id);
+    async delete(id): Promise<boolean> {
+        const existingUser = await this.findById(id);
 
-    if (!existingUser) {
-      throw new HttpException(
-        { message: 'User not found to update' },
-        HttpStatus.NOT_FOUND,
-      );
-    } else {
-      const updateResult = await this.utilsService.updateQuery(
-        'user', // Table name
-        payload, // Fields to update
-        { id: parseInt(id, 10) }, // Condition for the update
-      );
-      return updateResult
-        ? { message: 'User updated' }
-        : { message: 'User updation failed' };
-    }
-  }
+        if (!existingUser) {
+            throw new HttpException({ message: 'User not found to update' }, HttpStatus.NOT_FOUND);
+        }
 
-  async findAll(): Promise<User[]> {
-    const users = await this.prismaService.$queryRawUnsafe<User[]>(
-      `SELECT name , email , bio ,gender , preferences FROM "user";`,
-    );
-    if (!users.length) {
-      throw new HttpException(
-        { message: 'User details not found' },
-        HttpStatus.NOT_FOUND,
-      );
-    }
-    return users;
-  }
-
-  async findById(id): Promise<User> {
-    if (typeof id == 'string') {
-      id = parseInt(id, 10);
-    }
-    const user = await this.utilsService.querySingle<User>(
-      `SELECT id , name , email , number ,bio , gender ,preferences FROM "user" WHERE id = $1`,
-      id,
-    );
-
-    if (!user) {
-      throw new HttpException(
-        { message: 'User not found' },
-        HttpStatus.NOT_FOUND,
-      );
-    } else {
-      return user;
-    }
-  }
-
-  async delete(id): Promise<boolean> {
-    const existingUser = await this.findById(id);
-
-    if (!existingUser) {
-      throw new HttpException(
-        { message: 'User not found to update' },
-        HttpStatus.NOT_FOUND,
-      );
+        const deleted = await this.prismaService.$queryRawUnsafe(
+            `DELETE FROM "user" WHERE id = ${id} RETURNING id AS id;`
+        );
+        return deleted && deleted[0].id ? true : false;
     }
 
-    const deleted = await this.prismaService.$queryRawUnsafe(
-      `DELETE FROM "user" WHERE id = ${id} RETURNING id AS id;`,
-    );
-    return deleted && deleted[0].id ? true : false;
-  }
+    async hashPassword(password: string): Promise<string> {
+        return await argon2.hash(password);
+    }
 
-  async hashPassword(password: string): Promise<string> {
-    return await argon2.hash(password);
-  }
-
-  async verifyPassword(hash: string, password: string): Promise<boolean> {
-    return await argon2.verify(hash, password);
-  }
+    async verifyPassword(hash: string, password: string): Promise<boolean> {
+        return await argon2.verify(hash, password);
+    }
 }
